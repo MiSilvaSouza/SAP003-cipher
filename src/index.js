2af05bdd751f8ec6ef5279ad30bd9038863a0d56
MiSilvaSouza <mirian.s2@hotmail.com>
2019-08-20 13:35:39 -0300

    alterações nos arquivos

---
+++ src/index.js
@@ -0,0 +1,8 @@
+let chave = document.getElementById("chave")
+let mensagem = document.getElementById("msg")
+function cifrar() {
+    window.cipher.encode()
+  }
+function decifrar() {
+    window.cipher.decode()
+}
