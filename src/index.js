cb0c616d9f019d82cf971f08726f113d2513a3ec
MiSilvaSouza <mirian.s2@hotmail.com>
2019-08-24 19:05:25 -0300

    alterações no CSS

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const mensagem = document.getElementById("msg");
 
 document.getElementById("btnCifrar").onclick = function cifrar() {
   window.cipher.encode(chave.value, mensagem.value);
-  document.getElementById("resultcifra").innerHTML = window.cipher.encode(Number(chave.value), mensagem.value);
+  document.getElementById("result").innerHTML = window.cipher.encode(Number(chave.value), mensagem.value);
 };
 document.getElementById("btnDecifrar").onclick = function decifrar() {
   window.cipher.decode(chave.value, mensagem.value);
-  document.getElementById("resultdecifra").innerHTML = window.cipher.decode(Number(chave.value), mensagem.value);
+  document.getElementById("result").innerHTML = window.cipher.decode(Number(chave.value), mensagem.value);
 };
