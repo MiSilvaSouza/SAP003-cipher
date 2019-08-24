5d602fcf747c0af8b75c654394ccd267a773a54d
MiSilvaSouza <mirian.s2@hotmail.com>
2019-08-24 17:30:29 -0300

    arquivos corrigidos

--- src/cipher.js
+++ src/cipher.js
@@ -6,25 +6,23 @@ window.cipher = {
 };
 function encode(chave, mensagem) {
   let msg = mensagem.toUpperCase(); 
-  let msgFinal = ''  
-  for(let i = 0; i < msg.length; i++) {
+  let msgFinal = "";  
+  for (let i = 0; i < msg.length; i++) {
     let msgASC = msg.charCodeAt(i);
     let encodeMSG = ((msgASC - 65 + chave) % 26) + 65;
-    let msgCifrada = String.fromCharCode(encodeMSG)
-    msgFinal += msgCifrada        
+    let msgCifrada = String.fromCharCode(encodeMSG);
+    msgFinal += msgCifrada;                
   }
-return msgFinal
-  }
+  return msgFinal;
+}
 function decode(chave, mensagem) {
   let msg = mensagem.toUpperCase();
-  let msgFinal2 = ''
-  for(let i = 0; i < msg.length; i++) {
+  let msgFinal2 = "";
+  for (let i = 0; i < msg.length; i++) {
     let msgASC2 = msg.charCodeAt(i);
-    let decodeMSG = ((msgASC2 - 65 - chave) % 26) + 65;
-    let msgDecifrada = String.fromCharCode(decodeMSG)
-    msgFinal2 += msgDecifrada
+    let decodeMSG = ((msgASC2 - 90 - chave) % 26) + 90;
+    let msgDecifrada = String.fromCharCode(decodeMSG);
+    msgFinal2 += msgDecifrada;     
   }
-return msgFinal2
-
-  //alert (`A chave é ${chave} e a mensagem é ${mensagem}`)
+  return msgFinal2;  
 } 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const chave = document.getElementById("chave")
-const mensagem = document.getElementById("msg")
+const chave = document.getElementById("chave");
+const mensagem = document.getElementById("msg");
 
-function cifrar() {
-    window.cipher.encode(chave.value, mensagem.value)
-    document.getElementById("resultcifra").innerHTML = window.cipher.encode(chave.value, mensagem.value);
-  }
-function decifrar() {
-    window.cipher.decode(chave.value, mensagem.value)
-    document.getElementById("resultdecifra").innerHTML = window.cipher.decode(chave.value, mensagem.value);
-}
+document.getElementById("btnCifrar").onclick = function cifrar() {
+  window.cipher.encode(chave.value, mensagem.value);
+  document.getElementById("resultcifra").innerHTML = window.cipher.encode(Number(chave.value), mensagem.value);
+};
+document.getElementById("btnDecifrar").onclick = function decifrar() {
+  window.cipher.decode(chave.value, mensagem.value);
+  document.getElementById("resultdecifra").innerHTML = window.cipher.decode(Number(chave.value), mensagem.value);
+};
