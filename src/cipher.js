b7f050a6f3006b43c82aeaac4df9147a15ed760b
MiSilvaSouza <mirian.s2@hotmail.com>
2019-08-26 15:55:30 -0300

    alterações com hacker edition

--- src/cipher.js
+++ src/cipher.js
@@ -4,25 +4,33 @@ window.cipher = {
   // ...
 
 };
-function encode(chave, mensagem) {
-  let msg = mensagem.toUpperCase(); 
-  let msgFinal = "";  
-  for (let i = 0; i < msg.length; i++) {
-    let msgASC = msg.charCodeAt(i);
-    let encodeMSG = ((msgASC - 65 + chave) % 26) + 65;
-    let msgCifrada = String.fromCharCode(encodeMSG);
-    msgFinal += msgCifrada;                
+function encode(chave, mensagem) {  
+  let resultado = "";  
+  for (let i = 0; i < mensagem.length; i++) {
+    if (mensagem.charCodeAt(i) >=65 && mensagem.charCodeAt(i) <=90) {
+      resultado += String.fromCharCode((((mensagem.charCodeAt(i) - 65 + chave) % 26) + 65));    
+    } 
+    else if (mensagem.charCodeAt(i) >=97 && mensagem.charCodeAt(i) <=122) {
+      resultado += String.fromCharCode((((mensagem.charCodeAt(i) - 97 + chave) % 26) + 97));
+    } 
+    else if (mensagem.charCodeAt(i) >=32 && mensagem.charCodeAt(i) <=64) {
+      resultado += String.fromCharCode(mensagem.charCodeAt(i));
+    }       
   }
-  return msgFinal;
+  return resultado;
 }
-function decode(chave, mensagem) {
-  let msg = mensagem.toUpperCase();
-  let msgFinal2 = "";
-  for (let i = 0; i < msg.length; i++) {
-    let msgASC2 = msg.charCodeAt(i);
-    let decodeMSG = ((msgASC2 - 90 - chave) % 26) + 90;
-    let msgDecifrada = String.fromCharCode(decodeMSG);
-    msgFinal2 += msgDecifrada;     
+function decode(chave, mensagem) {  
+  let resultado = "";
+  for (let i = 0; i < mensagem.length; i++) {
+    if (mensagem.charCodeAt(i) >=65 && mensagem.charCodeAt(i) <=90) {
+      resultado += String.fromCharCode((((mensagem.charCodeAt(i) - 90 - chave) % 26) + 90));    
+    } 
+    else if (mensagem.charCodeAt(i) >=97 && mensagem.charCodeAt(i) <=122) {
+      resultado += String.fromCharCode((((mensagem.charCodeAt(i) - 122 - chave) % 26) + 122));
+    } 
+    else if (mensagem.charCodeAt(i) >=32 && mensagem.charCodeAt(i) <=64) {
+      resultado += String.fromCharCode(mensagem.charCodeAt(i));
+    }       
   }
-  return msgFinal2;  
-} 
+  return resultado;
+}
