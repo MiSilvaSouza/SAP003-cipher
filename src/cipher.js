ebdb5a5a02ae2d67a8d3d1ec3cb9f62203c7ad40
MiSilvaSouza <mirian.s2@hotmail.com>
2019-09-05 17:39:07 -0300

    alterações após feedback

--- src/cipher.js
+++ src/cipher.js
@@ -4,33 +4,43 @@ window.cipher = {
   // ...
 
 };
-function encode(chave, mensagem) {  
-  let resultado = "";  
-  for (let i = 0; i < mensagem.length; i++) {
-    if (mensagem.charCodeAt(i) >=65 && mensagem.charCodeAt(i) <=90) {
-      resultado += String.fromCharCode((((mensagem.charCodeAt(i) - 65 + chave) % 26) + 65));    
-    } 
-    if (mensagem.charCodeAt(i) >=97 && mensagem.charCodeAt(i) <=122) {
-      resultado += String.fromCharCode((((mensagem.charCodeAt(i) - 97 + chave) % 26) + 97));
-    } 
-    else if (mensagem.charCodeAt(i) >=32 && mensagem.charCodeAt(i) <=64) {
-      resultado += String.fromCharCode(mensagem.charCodeAt(i));
-    }       
+function encode(chave, mensagem) {
+  let resultado = "";
+  for (letra of mensagem) {
+    const msgCode = letra.charCodeAt();
+    const maiusculas = (msgCode >= 65 && msgCode <= 90);
+    const minusculas = (msgCode >= 97 && msgCode <= 122);
+    const caracteres = (msgCode >= 32 && msgCode <= 64);
+
+    if (maiusculas) {
+      resultado += String.fromCharCode((((msgCode - 65 + chave) % 26) + 65));
+    }
+    if (minusculas) {
+      resultado += String.fromCharCode((((msgCode - 97 + chave) % 26) + 97));
+    }
+    else if (caracteres) {
+      resultado += String.fromCharCode(msgCode);
+    }
   }
   return resultado;
 }
-function decode(chave, mensagem) {  
+function decode(chave, mensagem) {
   let resultado = "";
-  for (let i = 0; i < mensagem.length; i++) {
-    if (mensagem.charCodeAt(i) >=65 && mensagem.charCodeAt(i) <=90) {
-      resultado += String.fromCharCode((((mensagem.charCodeAt(i) - 90 - chave) % 26) + 90));    
-    } 
-    if (mensagem.charCodeAt(i) >=97 && mensagem.charCodeAt(i) <=122) {
-      resultado += String.fromCharCode((((mensagem.charCodeAt(i) - 122 - chave) % 26) + 122));
-    } 
-    else if (mensagem.charCodeAt(i) >=32 && mensagem.charCodeAt(i) <=64) {
-      resultado += String.fromCharCode(mensagem.charCodeAt(i));
-    }       
+  for (letra of mensagem) {
+    const msgCode = letra.charCodeAt();
+    const maiusculas = (msgCode >= 65 && msgCode <= 90);
+    const minusculas = (msgCode >= 97 && msgCode <= 122);
+    const caracteres = (msgCode >= 32 && msgCode <= 64);
+
+    if (maiusculas) {
+      resultado += String.fromCharCode((((msgCode - 90 - chave) % 26) + 90));
+    }
+    if (minusculas) {
+      resultado += String.fromCharCode((((msgCode - 122 - chave) % 26) + 122));
+    }
+    else if (caracteres) {
+      resultado += String.fromCharCode(msgCode);
+    }
   }
   return resultado;
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const chave = document.getElementById("chave");
 const mensagem = document.getElementById("msg");
 
-document.getElementById("btnCifrar").onclick = function cifrar() {
+document.getElementById("btn-cifrar").onclick = function cifrar() {
   window.cipher.encode(chave.value, mensagem.value);
-  document.getElementById("resultado").innerHTML = window.cipher.encode(Number(chave.value), mensagem.value);
+  document.getElementById("resultado").innerHTML = window.cipher.encode(+(chave.value), mensagem.value);
 };
-document.getElementById("btnDecifrar").onclick = function decifrar() {
+document.getElementById("btn-decifrar").onclick = function decifrar() {
   window.cipher.decode(chave.value, mensagem.value);
-  document.getElementById("resultado").innerHTML = window.cipher.decode(Number(chave.value), mensagem.value);
+  document.getElementById("resultado").innerHTML = window.cipher.decode(+(chave.value), mensagem.value);
 };
